ad0f06c4364baccd4c4701e8437483f8e0aeeba3
Adam Riggs <adamriggs@Blandines-iMac.local>
2018-08-28 17:43:51 -0400

    latest

--- wp-content/themes/Rare-Signals/page-home.php
+++ wp-content/themes/Rare-Signals/page-home.php
@@ -3,16 +3,61 @@
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 
-		<div class="splash">
+		<section class="splash">
 			<img class="splash__img" src="<?php echo get_template_directory_uri(); ?>/assets/img/header.png">
 			<!-- <img class="splash__video" src="<?php echo get_template_directory_uri(); ?>/assets/img/header.png"> -->
-		</div>
+		</section>
 
-		<div class="intro">
+		<dsection class="intro">
 			<h1>Transatlantic Plate Reverb</h1>
 
 			<span>Try</span><span>Buy</span>
-		</div>
+
+			<div><p>
+				Rare Signals presents a new standard in Plate Reverb plug-ins modeled from the finest analog plates of Europe and The U.S. With 24 independent decay settings, and reverb tails that remain un-manipulated, preserving the natural sustain and stereo spread of the actual analog giants. Beautifully presented to you in a simple and powerful design.
+			</p></div>
+		</section>
+
+		<section class="info">
+			<nav class="info__nav">
+				<a href="">Operation</a>
+				<a href="">Reverb Tails</a>
+				<a href="">Vintage Gear</a>
+			</nav>
+
+			<div class="info__content">
+
+				<h2 class="info__content__title">Operation</h2>
+
+				<p class="info__content__text">
+					The Transatlantic Reverb Plate was designed with simplicity as a paramount feature. We deliberately made the functions and the layout of controls as intuitive as possible. We love the idea that you can begin using it right away...without scrolling through the pages of a manual. That being said, there are some functions regarding automation features, details about the modes and decay properties...as well as some tips for CPU optimization that might interest you. These are explained in the manual.
+				</p>
+
+			</div>
+		</section>
+
+		<section class="diagram">
+			<img class="diagram__image" src="<?php echo get_template_directory_uri(); ?>/assets/img/Transatlantic.jpg">
+		</section>
+
+		<section class="reverb">
+			<h2 class="reverb__title">Reverb Trails</h2>
+
+			<img class="reverb__diagram" src="<?php echo get_template_directory_uri(); ?>/assets/img/reverb.png">
+
+			<p class="reverb__text">
+				The Transatlantic Plate Reverb was modeled directly from the finest examples of plates from Europe and the U.S. There is something very special about the way a perfectly tuned plate decays. Unfortunately it's not uncommon for some plugin manufacturers to manipulate the impulses used in the modeling process to make sense mathematically in a certain user interface. This manipulation allows for decay control settings to be precise (1 sec., 1.5 sec., 2 sec. and so on). 
+			</p>
+
+			<p class="reverb__text">
+				When dealing with an analog plate in the real world, decay times are hard to predict, let alone make perfectly even (value-wise). Manipulating reverb tails to fit conveniently into a user interface just sounds wrong (literally). You'll notice that the decay times on the Transatlantic reflect the true decay time of any particular setting. When switching between the EU/US modes, the current decay setting will jump to the closest value in the new mode. The result in sound quality far outweighs the ability to select even numbers on a dial. 
+			</p>
+		</section>
+
+		<section class="testimonials">
+			<div class="testimonials__slideshow">
+			</div>
+		</section>
 
 	</main>
 </div>
