b18ffaed3a4578dbed21b1a17ee8b2f591aa194a
Adam Riggs <adamriggs@Blandines-iMac.local>
2018-08-29 18:11:04 -0400

    removed all remaining font files

--- wp-content/themes/Rare-Signals/footer.php
+++ wp-content/themes/Rare-Signals/footer.php
@@ -10,10 +10,15 @@
 					<form action="https://raresignals.us12.list-manage.com/subscribe/post?u=51a73de7389fee59fc2ec2c7c&amp;id=f71447d3d9" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 					    <div id="mc_embed_signup_scroll">
 						
-						<input type="email" value="" name="EMAIL" class="email footer__top__signup__input" id="mce-EMAIL" placeholder="Join the Newsletter" required>
-					    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-					    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_51a73de7389fee59fc2ec2c7c_f71447d3d9" tabindex="-1" value=""></div>
-					    <div class="clear footer__top__signup__submit__container"><input type="submit" value="k" name="subscribe" id="mc-embedded-subscribe" class="button footer__top__signup__submit"></div>
+							<input type="email" value="" name="EMAIL" class="email footer__top__signup__input" id="mce-EMAIL" placeholder="Join the Newsletter" required>
+						    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+						    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_51a73de7389fee59fc2ec2c7c_f71447d3d9" tabindex="-1" value=""></div>
+						    <div class="clear footer__top__signup__submit__container">
+						    	<label>
+						    	<input type="submit" value="" name="subscribe" id="mc-embedded-subscribe" class="button footer__top__signup__submit">
+						    	<img class="footer__top__signup__button" src="<?php echo get_template_directory_uri(); ?>/assets/img/external_link.svg">
+						    	<label>
+						    </div>
 					    </div>
 					</form>
 					</div>
