d15b060f487a841d902b00fd195aa38f0333546a
Adam Riggs <adamriggs@Blandines-iMac.local>
2018-08-29 17:26:35 -0400

    font stuff

--- wp-content/themes/Rare-Signals/header.php
+++ wp-content/themes/Rare-Signals/header.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width" />
     <title><?php wp_title(); ?></title>
     <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
+    <link type="text/css" rel="stylesheet" href="https://fast.fonts.net/cssapi/7e511114-e840-48b4-8daf-6db4881903f5.css"/>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js" integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ" crossorigin="anonymous"></script>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
