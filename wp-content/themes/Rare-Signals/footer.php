c75885d54ea367ef8e614d316034a18c5d2a89d7
Adam Riggs <adamriggs@Blandines-iMac.local>
2018-08-29 14:42:22 -0400

    looking good

--- wp-content/themes/Rare-Signals/footer.php
+++ wp-content/themes/Rare-Signals/footer.php
@@ -1,6 +1,37 @@
 
         <footer class="footer" role="contentinfo">
 
+        	<div class="footer__top">
+	        	<div class="footer__top__logo"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/footer-logo.png"></div>
+
+	        	<div class="footer__top__signup">
+	        		<!-- Begin MailChimp Signup Form -->
+					<div id="mc_embed_signup">
+					<form action="https://raresignals.us12.list-manage.com/subscribe/post?u=51a73de7389fee59fc2ec2c7c&amp;id=f71447d3d9" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+					    <div id="mc_embed_signup_scroll">
+						
+						<input type="email" value="" name="EMAIL" class="email footer__top__signup__input" id="mce-EMAIL" placeholder="Join the Newsletter" required>
+					    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+					    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_51a73de7389fee59fc2ec2c7c_f71447d3d9" tabindex="-1" value=""></div>
+					    <div class="clear footer__top__signup__submit__container"><input type="submit" value="k" name="subscribe" id="mc-embedded-subscribe" class="button footer__top__signup__submit"></div>
+					    </div>
+					</form>
+					</div>
+
+					<!--End mc_embed_signup-->
+	        	</div>
+
+	        	<div class="footer__top__social">
+	        		<ul>
+	        			<li><a href="http://facebook.com/raresignals">f</></li>
+	        			<li><a href="http://twitter.com/raresignals">t</a></li>
+	        			<li><a href="http://instagram.com/raresignals">c</a></li>
+	        		</ul>
+	        	</div>
+	        </div>
+
+        	<div class="footer__legal">©2018 Rare Signals. All rights reserved.</div>
+
         </footer>
     </div>
     <?php wp_footer(); ?>
--- wp-content/themes/Rare-Signals/page-home.php
+++ wp-content/themes/Rare-Signals/page-home.php
@@ -4,57 +4,71 @@
 	<main id="main" class="site-main" role="main">
 
 		<section class="splash">
-			<img class="splash__img" src="<?php echo get_template_directory_uri(); ?>/assets/img/header.png">
-			<!-- <img class="splash__video" src="<?php echo get_template_directory_uri(); ?>/assets/img/header.png"> -->
+			<!-- <img class="splash__img" src="<?php echo get_template_directory_uri(); ?>/assets/img/header.png"> -->
+			<video autoplay loop class="splash__video">
+				<source class="splash__video" src="<?php echo get_template_directory_uri(); ?>/assets/vid/loopingbanner.mp4">
+			</video>
 		</section>
 
-		<dsection class="intro">
-			<h1>Transatlantic Plate Reverb</h1>
+		<section class="intro">
+			<a class="intro__play" href=""><img class="intro__play__button" src="<?php echo get_template_directory_uri(); ?>/assets/img/play.svg"></a>
+			<h1 class="intro__header">Transatlantic Plate Reverb</h1>
 
-			<span>Try</span><span>Buy</span>
+			<div class="intro__link__container">
+				<a href="https://www.raresignals.com/store-test/" class="intro__link intro__link__buy">Buy</a>
+				<a href="" class="intro__link intro__link__try">Try</a>
+			</div>
 
 			<div><p>
 				Rare Signals presents a new standard in Plate Reverb plug-ins modeled from the finest analog plates of Europe and The U.S. With 24 independent decay settings, and reverb tails that remain un-manipulated, preserving the natural sustain and stereo spread of the actual analog giants. Beautifully presented to you in a simple and powerful design.
 			</p></div>
 		</section>
 
-		<section class="info">
-			<nav class="info__nav">
-				<a href="">Operation</a>
-				<a href="">Reverb Tails</a>
-				<a href="">Vintage Gear</a>
+		<section class="nav">
+			<nav class="nav__container">
+				<a class="nav__item" href="#operation">Operation</a>
+				<a class="nav__item" href="#reverb">Reverb Tails</a>
+				<a class="nav__item" href="#testimonials">Testimonials</a>
 			</nav>
+		</section>
 
-			<div class="info__content">
+		<section id="operation" class="operation">
+			<div class="operation__content">
+				<h2 class="operation__content__title">Operation</h2>
 
-				<h2 class="info__content__title">Operation</h2>
-
-				<p class="info__content__text">
+				<p class="operation__content__text">
 					The Transatlantic Reverb Plate was designed with simplicity as a paramount feature. We deliberately made the functions and the layout of controls as intuitive as possible. We love the idea that you can begin using it right away...without scrolling through the pages of a manual. That being said, there are some functions regarding automation features, details about the modes and decay properties...as well as some tips for CPU optimization that might interest you. These are explained in the manual.
 				</p>
-
 			</div>
 		</section>
 
 		<section class="diagram">
-			<img class="diagram__image" src="<?php echo get_template_directory_uri(); ?>/assets/img/Transatlantic.jpg">
+			<img class="diagram__image" src="<?php echo get_template_directory_uri(); ?>/assets/img/Diagram.png">
 		</section>
 
-		<section class="reverb">
+		<section id="reverb" class="reverb">
 			<h2 class="reverb__title">Reverb Trails</h2>
 
-			<img class="reverb__diagram" src="<?php echo get_template_directory_uri(); ?>/assets/img/reverb.png">
+			<div class="reverb__diagram">
+				<div class="reverb__diagram__left"></div>
+				<div class="reverb__diagram__right">
+					<img class="reverb__diagram__right__image" src="<?php echo get_template_directory_uri(); ?>/assets/img/reverb.png">
+					<p class="reverb__diagram__right__text">How reverb tails travel through space.</p>
+				</div>
+			</div>
 
-			<p class="reverb__text">
-				The Transatlantic Plate Reverb was modeled directly from the finest examples of plates from Europe and the U.S. There is something very special about the way a perfectly tuned plate decays. Unfortunately it's not uncommon for some plugin manufacturers to manipulate the impulses used in the modeling process to make sense mathematically in a certain user interface. This manipulation allows for decay control settings to be precise (1 sec., 1.5 sec., 2 sec. and so on). 
-			</p>
+			<div class="reverb__text">
+				<p class="reverb__text__left">
+					The Transatlantic Plate Reverb was modeled directly from the finest examples of plates from Europe and the U.S. There is something very special about the way a perfectly tuned plate decays. Unfortunately it's not uncommon for some plugin manufacturers to manipulate the impulses used in the modeling process to make sense mathematically in a certain user interface. This manipulation allows for decay control settings to be precise (1 sec., 1.5 sec., 2 sec. and so on). 
+				</p>
 
-			<p class="reverb__text">
-				When dealing with an analog plate in the real world, decay times are hard to predict, let alone make perfectly even (value-wise). Manipulating reverb tails to fit conveniently into a user interface just sounds wrong (literally). You'll notice that the decay times on the Transatlantic reflect the true decay time of any particular setting. When switching between the EU/US modes, the current decay setting will jump to the closest value in the new mode. The result in sound quality far outweighs the ability to select even numbers on a dial. 
-			</p>
+				<p class="reverb__text__right">
+					When dealing with an analog plate in the real world, decay times are hard to predict, let alone make perfectly even (value-wise). Manipulating reverb tails to fit conveniently into a user interface just sounds wrong (literally). You'll notice that the decay times on the Transatlantic reflect the true decay time of any particular setting. When switching between the EU/US modes, the current decay setting will jump to the closest value in the new mode. The result in sound quality far outweighs the ability to select even numbers on a dial. 
+				</p>
+			</div>
 		</section>
 
-		<section class="testimonials">
+		<section id="testimonials" class="testimonials">
 			<div class="testimonials__slideshow">
 			</div>
 		</section>
